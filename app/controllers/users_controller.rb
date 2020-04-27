5c40549bb81e6961a02353fafd5a5b7ed579c1cd
Genki Nishiyama <genki.nishiyama@gmail.com>
2020-04-27 23:28:18 +0900

    add editing function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,16 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user =  User.find(params[:id])
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
   end
 
   private
